0bbfd1590b1446ccbf63e33b2c058c9b01a0fae1
jclawson <jclawson@qualys.com>
2012-07-09 14:05:06 -0600

    fix possible NPE

--- src/main/java/com/succinctllc/hazelcast/work/bundler/DeferredWorkBundler.java
+++ src/main/java/com/succinctllc/hazelcast/work/bundler/DeferredWorkBundler.java
@@ -230,7 +230,7 @@ public class DeferredWorkBundler<I> {
     	
     	List<I> bundle = groupItemBuffer.getAsList(group);
         
-        if(bundle.size() == 0) {
+        if(bundle == null || bundle.size() == 0) {
             return 0;
         }
         
